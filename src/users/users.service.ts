3670cb650eb620c0714240d0918c641a7d7fc47f
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-09-05 23:56:05 +0300

    feat(create user)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import { hashSync } from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
 
   constructor(
-    @InjectModel(User.name) private readonly userModel: Model<User>
+
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+
   ) { }
 
-  create(createUserDto: CreateUserDto) {
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
 
+  async create(createUserDto: CreateUserDto) {
+
+    const existsUser = await this.findByEmail(createUserDto.email);
+
+    if (existsUser) throw new BadRequestException('User already registed');
+
+    const user = new this.userModel(createUserDto);
+
+    user.password = hashSync(createUserDto.password);
+
+    await user.save();
+
+    return user;
   }
 
   findAll() {
