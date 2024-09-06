e1d78579862b20f9e7abde6d1be9033ce0344018
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-09-06 14:11:31 +0300

    feat(messages): to create message's crud (not implemented yet)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot('mongodb://root:example@localhost:27017/db_chats?authSource=admin'),
     AuthModule,
     UsersModule,
+    MessagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
