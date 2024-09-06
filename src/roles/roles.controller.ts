f228c95f7277497b80d34873a588a634a18f6123
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-09-06 18:52:30 +0300

    feat(roles): role's CRUD implemented

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -5,7 +5,7 @@ import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('roles')
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
+  constructor(private readonly rolesService: RolesService) { }
 
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
@@ -17,18 +17,18 @@ export class RolesController {
     return this.rolesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.rolesService.findOne(term);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  @Patch(':term')
+  update(@Param('term') term: string, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.rolesService.update(term, updateRoleDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  @Delete(':term')
+  remove(@Param('term') term: string) {
+    return this.rolesService.remove(term);
   }
 }
