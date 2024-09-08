09c03a03ed7653e6f9b7555eb9eb926b873b8f13
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-09-08 23:45:50 +0300

    refact

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -10,6 +11,50 @@ export class AuthService {
 
   findAll() {
     return `This action returns all auth`;
+=======
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { AuthResponse } from './auth-response/auth-response.interface';
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async register(createUserDto: CreateUserDto): Promise<AuthResponse> {
+
+    const user = await this.usersService.create(createUserDto);
+    const token = this.jwtService.sign({ id: user.id });
+
+    return { user, token };
+  }
+
+  async login(username: string, password: string): Promise<any> {
+    // Buscar el usuario por nombre de usuario
+    const user = await this.usersService.findByUsername(username);
+    // Verificar si el usuario existe
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // Verificar la contraseña
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // Si la contraseña es inválida, lanzar excepción
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // Generar y retornar el JWT
+    const payload = { id: user.id, username: user.username };
+    return this.jwtService.sign(payload);
+>>>>>>> 967c35c (refact)
   }
 
   findOne(id: number) {
